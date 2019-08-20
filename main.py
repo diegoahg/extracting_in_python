319f871b782178b68e4d07ef14df203cdc35bdaa
DieGO! <diegohernandez.com@gmail.com>
2019-08-20 10:14:48 -0400

    master - add comments and encodings

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def clean_title(df):
     return df
 
 def clean_data(df):
-     """
+    """
     Clean all dataFrame of caracters unnesesary. Then cast the colums to specific
     data type.
 
@@ -42,19 +42,24 @@ def clean_data(df):
 
     return df
 
-def get_total_population(df):
-    return df['Población'].sum()
+def set_percentage_population(df):
+    """
+    Include a new column named "Porcentaje de Población" that contain the percentage
+    of population each region.
 
-def set_percentage_population(df, total):
+    Parameters
+        ----------
+        df : is the DataFrame that will be modificated and returned
+    """
+    total = df['Población'].sum()
     df["Porcentaje de Población"] = round(df["Población"]/total * 100,2)
     df["Porcentaje de Población"] = df["Porcentaje de Población"].astype(str)
     df["Porcentaje de Población"] = df["Porcentaje de Población"] + "%"
     return df
 
-
-print("Init Program...")
+# Main of the de program
 print("Getting data from wiki Chile...")
-dfs = pd.read_html('https://es.wikipedia.org/wiki/Chile', header = 1, attrs={'class': 'wikitable col1izq col2der col3der col4der col5der col6izq'}, encoding='latin-1')
+dfs = pd.read_html('https://es.wikipedia.org/wiki/Chile', header = 1, attrs={'class': 'wikitable col1izq col2der col3der col4der col5der col6izq'}, encoding='utf-8')
 df = dfs[0]
 
 print("Cleaning data")
@@ -62,10 +67,10 @@ df = clean_title(df)
 df = clean_data(df)
 
 print("Calculating the population percentage...")
-total = get_total_population(df)
-df = set_percentage_population(df, total)
-print(df)
+df = set_percentage_population(df)
 
-df.to_csv(r'organizacion_territoral_de_chile.csv', index=False, encoding='latin-1')
-# env
+print(df)
+df.to_csv(r'organizacion_territoral_de_chile.csv', index=False, encoding='utf-8')
+# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
 # pip install -r requirements.txt
+# Python >=3.5.3
